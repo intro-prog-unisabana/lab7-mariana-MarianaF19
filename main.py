29e8ce2515aa930b6e63c7bf18cfee963e9ea896
Mariana F <marianafama@unisabana.edu.co>
2026-04-21 13:29:17 -0500

    14

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ from password_manager import add_login, change_password, encrypt_passwords_in_fi
 
 
 def main() -> None:
- filename = input("Enter the CSV file name: ")
-encrypt_passwords_in_file(filename)
-while True:
+   from password_manager import encrypt_passwords_in_file, change_password, add_login
+
+def main():
+    filename = input("Enter the CSV file name: ")
+    encrypt_passwords_in_file(filename)
+
+    while True:
         # 2. Mostrar el menú de opciones
         print("Options: (1) Change Password, (2) Add Password, (3) Quit: ", end="")
         option = input()
+
         if option == "1":
             datos = input("Enter the website and the new password: ").split()
+            
             if len(datos) < 2:
                 print("Input is in the wrong format!")
             elif len(datos[1]) < 12:
                 print("Password is too short!")
             else:
-                website = datos[0] 
+                website = datos[0]
                 nueva_pass = datos[1]
                 if change_password(filename, website, nueva_pass):
                     print("Password changed.")
@@ -33,6 +39,7 @@ while True:
                 website, user, password = datos[0], datos[1], datos[2]
                 add_login(filename, website, user, password)
                 print("Login added.")
+
         elif option == "3":
             break
         
