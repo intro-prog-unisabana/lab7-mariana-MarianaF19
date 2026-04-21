b89975aa9ff4be062915ec2bd231f2d53ef3d393
Mariana F <marianafama@unisabana.edu.co>
2026-04-21 13:11:05 -0500

    7

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from password_manager import add_login, change_password, encrypt_passwords_in_fi
 
 
 def main() -> None:
-    """TODO: Parte 5 - programa principal interactivo."""
-    pass
+ filename = input("Enter the CSV file name: ")
+encrypt_passwords_in_file(filename)
+while True:
+        # 2. Mostrar el menú de opciones
+        print("Options: (1) Change Password, (2) Add Password, (3) Quit: ", end="")
+        option = input()
+pass
 
 
 if __name__ == "__main__":
